df207d8e491afdf69ca48013cdf24b30597b81bd
wate <pc.poisoning@gmail.com>
2021-08-14 21:50:30 +0900

    Redmineの設定のインポート処理を調整

---
+++ templates/settings/setting.rb
@@ -0,0 +1,13 @@
+#{% raw %}
+def import_setting(setting)
+  if setting.key?('default_projects_trackers')
+    setting['default_projects_tracker_ids'] = Tracker.where(:name => setting['default_projects_trackers']).pluck(:id).map {|item| item.to_s }
+    setting.delete('default_projects_trackers')
+  end
+  Setting.set_all_from_params(setting);
+end
+
+setting = {};
+setting = YAML.load_file('./tmp/import/setting.yml') if File.exists?('./tmp/import/setting.yml')
+import_setting(setting) if setting.present?
+#{% endraw %}
