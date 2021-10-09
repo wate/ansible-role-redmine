8fe9247139cb58546deffeec10a3c63416efb056
wate <pc.poisoning@gmail.com>
2021-10-10 01:02:30 +0900

    プラグインの設定のインポートに対応

--- templates/settings/setting.rb
+++ templates/settings/setting.rb
@@ -1,10 +1,19 @@
 #{% raw %}
 def import_setting(setting)
-  if setting.key?('default_projects_trackers')
-    setting['default_projects_tracker_ids'] = Tracker.where(:name => setting['default_projects_trackers']).pluck(:id).map {|item| item.to_s }
-    setting.delete('default_projects_trackers')
+  redmine_setting = setting.reject {|key, value| key.start_with?('plugin_') }
+  if redmine_setting.key?('default_projects_trackers')
+    redmine_setting['default_projects_tracker_ids'] = Tracker.where(:name => redmine_setting['default_projects_trackers']).pluck(:id).map {|item| item.to_s }
+    redmine_setting.delete('default_projects_trackers')
   end
-  Setting.set_all_from_params(setting);
+  if redmine_setting.present?
+    Setting.set_all_from_params(redmine_setting)
+  end
+  plugin_settings = setting.select {|key, value| key.start_with?('plugin_') }
+  if plugin_settings.present?
+    plugin_settings.each do |plugin_name, plugin_setting|
+      Setting.send plugin_name + '=', plugin_setting.with_indifferent_access
+    end
+  end
 end
 
 setting = {};
