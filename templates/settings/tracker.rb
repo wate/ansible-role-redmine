1231fdc816ad83cf5c57e50f1a789bcbb5631e3e
wate <pc.poisoning@gmail.com>
2023-03-13 18:49:27 +0900

    実装漏れ

--- templates/settings/tracker.rb
+++ templates/settings/tracker.rb
@@ -26,6 +26,7 @@ def import_tracker(trackers)
       ]
     end
     tracker.core_fields = item['enabled_standard_fields'] if item['enabled_standard_fields'].present?
+    tracker.description = item['description'] if item.key?('description')
     tracker.default_status_id = default_issue_status.id
     if item['default_status'].present?
       status_name = item['default_status']
